b2dfa8bbf85d89515d3bd7cf60a372bd4badd4be
AnchalPandey29 <anchal29pandey@gmail.com>
2023-02-05 14:37:52 +0530

    first commit

--- frontend/src/components/main/Home.js
+++ frontend/src/components/main/Home.js
@@ -4,9 +4,11 @@ import './Home.css';
 const Home = () => {
   return (
      
-  <div>
+  <>
+  
       <div className="home-container pt-5" >
         <div className="row">
+          
           <div className="col-md-4">
             <h1>InvestUp</h1>
             <p>Explore the funding environment with InvestUp and tap into some of the most relevant models for your startup's growth.</p>
@@ -14,12 +16,14 @@ const Home = () => {
               Get started
             </button>
           </div>
-          <div className="col-md-4">
-            <img className='hero-image' src="\home.png" alt="" />
+          <div className="col-md-4 heroimg">
+            <img className='hero-image' src="\home.png" alt="" /> 
           </div>
-          
+          <img src="\wave.png" alt="" />
         </div>
+        
       </div>
+
 
       <div className='row'>
         <div className='col-md-3 p-1'>
@@ -27,7 +31,10 @@ const Home = () => {
                   <img className='card-image' src="\seed.png" alt="" />
                   {/* https://img.freepik.com/free-vector/plant-white_1308-41021.jpg?w=740&t=st=1674664226~exp=1674664826~hmac=dbbe97c9db1b898f9e8bffaa36507ff84122fb5d7306e590f6159b09f6affdae */}
                   <h3>Startup</h3>
-                  <p></p>
+                  <p><i class="fa fa-circle" aria-hidden="true"></i>
+                  <i class="fa fa-circle" aria-hidden="true"></i>
+                  <i class="fa fa-circle" aria-hidden="true"></i>
+                  </p>
             </div>
         </div>
 
@@ -35,17 +42,20 @@ const Home = () => {
             <div className='card'>
                   <img className='card-image' src="\plant.png" alt="" />
                   <h3>Startup</h3>
-                  
-                  <p></p>
+                    <p><i class="fa fa-circle" aria-hidden="true"></i>
+                    <i class="fa fa-circle" aria-hidden="true"></i>
+                    <i class="fa fa-circle" aria-hidden="true"></i>
+                     </p>
             </div>
         </div>
       </div>
 
       
-  </div>
+  </>
     
   
   )
 }
 
 export default Home;
+
