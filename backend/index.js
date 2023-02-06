5a017fc9937e2a574446450275d04f91d1d4c84f
AnchalPandey29 <anchal29pandey@gmail.com>
2023-02-06 19:46:01 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,13 @@ const app = express();
 
 app.use(express.json());
 // app.use(express.urlencoded({extended : true}));
-app.use('/user', userRouter);
 app.use(cors(
     {
-        origin : 'http://localhost:3000',
-        credentials : true
-    }
+        origin: ['http://localhost:3000', 'http://localhost:3001'],
+        credentials: true
+    },
 ));
+app.use('/user', userRouter);
 
 app.get('/', (req, res) => {
     console.log('Request at index');
