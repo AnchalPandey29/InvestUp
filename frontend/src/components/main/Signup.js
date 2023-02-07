c503f1b14e30479d99c44a68cdc2c4c495be44dd
SaumyaDixit15 <dixitsaumya47@gmail.com>
2023-02-07 19:57:02 +0530

    first commit

--- frontend/src/components/main/Signup.js
+++ frontend/src/components/main/Signup.js
@@ -1,77 +1,79 @@
-import React from 'react'
+import { Formik } from "formik";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Signup = () => {
+
+  const navigate = useNavigate();
+
+  const userSubmit = async (formdata, { setSubmitting }) => {
+    console.log(formdata);
+
+
+    // 1. URL
+    // 2. request method - get, post, put, delete , etc.
+    // 3. Data you want to sent.
+    // 4. data format - json, etc.
+
+    setSubmitting(true);
+    const res = await fetch("http://localhost:5000/user/add", {
+      method: "POST",
+      body: JSON.stringify(formdata),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    console.log(res.status)
+    setSubmitting(false);
+
+    if (res.status === 200) {
+      Swal.fire({
+        icon: "success",
+        title: 'Success',
+        text: 'You have registered successfully'
+      })
+      navigate('/login');
+    } else {
+      // error alert
+    }
+  }
+
   return (
     <div>
-      <form>
-  {/* 2 column grid layout with text inputs for the first and last names */}
-  <div className="row mb-4">
-    <div className="col">
-      <div className="form-outline">
-        <input type="text" id="form3Example1" className="form-control" />
-        <label className="form-label" htmlFor="form3Example1">
-          First name
-        </label>
-      </div>
-    </div>
-    <div className="col">
-      <div className="form-outline">
-        <input type="text" id="form3Example2" className="form-control" />
-        <label className="form-label" htmlFor="form3Example2">
-          Last name
-        </label>
-      </div>
-    </div>
-  </div>
-  {/* Email input */}
-  <div className="form-outline mb-4">
-    <input type="email" id="form3Example3" className="form-control" />
-    <label className="form-label" htmlFor="form3Example3">
-      Email address
-    </label>
-  </div>
-  {/* Password input */}
-  <div className="form-outline mb-4">
-    <input type="password" id="form3Example4" className="form-control" />
-    <label className="form-label" htmlFor="form3Example4">
-      Password
-    </label>
-  </div>
-  {/* Checkbox */}
-  <div className="form-check d-flex justify-content-center mb-4">
-    <input
-      className="form-check-input me-2"
-      type="checkbox"
-      defaultValue=""
-      id="form2Example33"
-      defaultChecked=""
-    />
-    <label className="form-check-label" htmlFor="form2Example33">
-      Subscribe to our newsletter
-    </label>
-  </div>
-  {/* Submit button */}
-  <button type="submit" className="btn btn-primary btn-block mb-4">
-    Sign up
-  </button>
-  {/* Register buttons */}
-  <div className="text-center">
-    <p>or sign up with:</p>
-    <button type="button" className="btn btn-secondary btn-floating mx-1">
-      <i className="fab fa-facebook-f" />
-    </button>
-    <button type="button" className="btn btn-secondary btn-floating mx-1">
-      <i className="fab fa-google" />
-    </button>
-    <button type="button" className="btn btn-secondary btn-floating mx-1">
-      <i className="fab fa-twitter" />
-    </button>
-    <button type="button" className="btn btn-secondary btn-floating mx-1">
-      <i className="fab fa-github" />
-    </button>
-  </div>
-</form>
+      <div className="col-md-3 mx-auto pt-5">
+        <div className="card">
+          <div className="card-body">
+            <p className="text-center h4">Signup Form</p>
+            <hr />
+            <Formik initialValues={{ name: "", email: "", password: "", age: "" }} onSubmit={userSubmit}>
+              {({ values, handleSubmit, handleChange, isSubmitting }) => (
+                <form onSubmit={handleSubmit}>
+                  <label>Name</label>
+                  <input value={values.name} onChange={handleChange} name="name" className="form-control" />
 
+                  <label>Email Address</label>
+                  <input value={values.email} onChange={handleChange} name="email" className="form-control" />
+
+                  <label>Age</label>
+                  <input type="number" value={values.age} onChange={handleChange} name="age" className="form-control" />
+
+                  <label>Password</label>
+                  <input className="form-control mb-3" type="password" value={values.password} onChange={handleChange} name="password" />
+
+                  <button disabled={isSubmitting} type="submit" className="btn btn-primary">
+                    {
+                      isSubmitting ?
+                        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                        :
+                        'Submit'
+                    }
+                  </button>
+                </form>
+              )}
+            </Formik>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
