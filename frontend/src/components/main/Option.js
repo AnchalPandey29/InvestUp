16a4b1b403978923ee662a9781c7da894811a6be
AnchalPandey29 <anchal29pandey@gmail.com>
2023-02-20 13:23:19 +0530

    .

--- frontend/src/components/main/Option.js
+++ frontend/src/components/main/Option.js
@@ -3,21 +3,21 @@ import { Navigate } from 'react-router-dom'
 
 const Option = () => {
     return (
-        <div className='mx-auto'>
-            <label htmlFor="">Are you a ..</label>
-            <div class="form-check">
+        <div className=' mx-auto'>
+          <h2>Are you a....</h2>
+            <div class="form-check mb-4 ps-5" >
                 <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
                 &nbsp;
                 <label class="form-check-label" for="flexRadioDefault1">Startup </label>
             </div>
 
-            <div class="form-check">
+            <div class="form-check mb-4 ps-5">
                 <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked />
                 &nbsp;
                 <label class="form-check-label" for="flexRadioDefault2">Investor </label>
             </div>
 
-            <div class="form-check">
+            <div class="form-check mb-4 ps-5">
                 <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked />
                 &nbsp;
                 <label class="form-check-label" for="flexRadioDefault2">Guest</label>
