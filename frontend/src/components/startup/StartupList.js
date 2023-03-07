83a72c5bcb68a636c7387c4e256bf87931f40e80
m-triple-m <triplem656@mail.com>
2023-03-07 13:48:41 +0530

    ..

--- frontend/src/components/startup/StartupList.js
+++ frontend/src/components/startup/StartupList.js
@@ -8,28 +8,34 @@ const StartupList = () => {
   const [startupList, setStartupList] = useState([]);
 
   const url = app_config.apiurl;
+  const placeholder = 'https://via.placeholder.com/640x400.png/a59090/000000?Text=640x400';
 
   const fetchData = async () => {
     const res = await fetch(url + "/startup/getall");
     const data = await res.json();
     console.log(data);
-    setStartupList( data.result.filter((user) => ( user.role === 'startup' )) );
+    setStartupList(data.result.filter((user) => user.role === "startup"));
     // setStartupList(data.result);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
-  return (
-    <div>
-      {startupList.map((startup) => (
-        <div>
-          <div
-            className="row card gx-5 mx-auto m-3"
-            style={{ width: "70%", height: "fit-content" }}
-          >
-            
-            <div className="col-md-6 mb-4">
+
+  const displayList = () => {
+    return startupList.map((startup) => (
+      <div className="card bg-warning mb-3">
+        <div className="row gx-5">
+          <div className="col-md-4">
+            <img
+              className="img-fluid"
+              src={startup.startupimage? (url + "/" + startup.startupimage): placeholder}
+              alt=""
+            />
+          </div>
+
+          <div className="col-md-6">
+            <div className="card-body">
               <h4>
                 <strong> {startup.name} </strong>
               </h4>
@@ -37,20 +43,27 @@ const StartupList = () => {
               <Link
                 type="button"
                 className="btn"
-                to={"/startup/startupdetails/"+startup._id}
+                to={"/main/startupdetails/" + startup._id}
                 style={{ backgroundColor: "#9c3353", color: "white" }}
               >
                 Read more
               </Link>
             </div>
-
           </div>
-
-          
         </div>
-      ))}
+      </div>
+    ));
+  };
 
-      
+  return (
+    <div>
+      <header>
+        <h1>Startup List</h1>
+      </header>
+      <div className="row">
+        <div className="col-3"></div>
+        <div className="col-9">{displayList()}</div>
+      </div>
     </div>
   );
 };
