820bbce649cebb928d6d37d0ff790759e0b0ff9c
m-triple-m <triplem656@mail.com>
2023-03-13 12:11:18 +0530

    ..

--- frontend/src/components/startup/Checkout.js
+++ frontend/src/components/startup/Checkout.js
@@ -1,31 +1,68 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom';
-import app_config from '../../config';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+import app_config from "../../config";
 // const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const Checkout = () => {
-
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(sessionStorage.getItem("startup"))
+  );
   const url = app_config.apiurl;
+  const navigate = useNavigate();
   // CHECKOUT_SESSION_ID
-  const {sessionid} = useParams();
-    console.log(sessionid);
 
-    const getPaymentDetails = async () => {
-      const response = await fetch(`${url}/util/getpaymentdetails/${sessionid}`);
-      console.log(response.status);
-      // if(res)
+  const addSubscription = async (subscriptionData) => {
+    const today = new Date();
+    const res = await fetch(`${url}/Subscription/add`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: currentUser._id,
+        startDate: today,
+        endDate: new Date(today.setMonth(today.getMonth() + 1)),
+        duration: "1 Month",
+        data: {
+          name: "Basic",
+          customer_details: subscriptionData.customer_details,
+        }
+      }),
+    });
+
+    if (res.status === 201) {
+      Swal.fire({
+        icon: "success",
+        title: "Subscribed",
+        text: "Purchase Successful",
+      });
+      navigate("/startup/dashboard");
+
+      const data = await res.json();
+    }
+  };
+
+  const { sessionid } = useParams();
+  console.log(sessionid);
+
+  const getPaymentDetails = async () => {
+    const response = await fetch(`${url}/util/getpaymentdetails/${sessionid}`);
+    console.log(response.status);
+    if (response.status === 200) {
       const data = await response.json();
       console.log(data);
+      if (data.payment_status === "paid") {
+        addSubscription(data);
+      }
     }
+  };
 
-    useEffect(() => {
-      getPaymentDetails();
-    }, [])
-    
+  useEffect(() => {
+    getPaymentDetails();
+  }, []);
 
-  return (
-    <div>Checkout</div>
-  )
-}
+  return <div>Checkout</div>;
+};
 
 export default Checkout;
