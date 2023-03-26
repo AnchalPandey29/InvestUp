615677aa44e11c5e8280e86665bcdc930090d5f0
m-triple-m <triplem656@gmail.com>
2023-03-26 15:16:40 +0530

    ..

--- backend/models/SubscriptionModel.js
+++ backend/models/SubscriptionModel.js
@@ -1,13 +1,11 @@
 const { Schema, model, Types } = require("../connection");
 
 const schema = new Schema({
-  user: { type:Types.ObjectId,ref : 'startup' },
-  startDate:Date,
-  endDate:Date,
-  duration:String,
-  data: Object
-  
-  });
-
+  user: { type: Types.ObjectId, ref: "startup" },
+  startDate: Date,
+  endDate: Date,
+  duration: String,
+  data: Object,
+});
 
 module.exports = model("subscription", schema);
